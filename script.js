755e7fe4e8275b29df7d1a6b8dc44dc6ec8f04a2
VSlobolinskyi <vitalii.slobolinskyi@gmail.com>
2020-10-22 13:53:15 +0300

    Standarizetion and menu
    Standartized and cleaned CSS, added proper class names, removed unnecesarry JS, made proper menu behaviour.

--- script.js
+++ script.js
@@ -1,50 +1,19 @@
 
-function main(){
-    let renome1 = document.querySelector("#headerImg");
-    let icons = document.querySelector("#headerIcons");
-    icons.style.left = window.innerWidth*0.94-icons.offsetWidth-parseInt(getComputedStyle(renome1).width) + "px";
-    icons.style.visibility = "visible";
-
-    let home = document.querySelector("#home");
-    let collage1 = document.querySelector("#collage1");
-    home.style.height = collage1.offsetHeight + "px";
-
-    let img = document.querySelector('#c1Img');
-    let wrapper = document.querySelector('#wrapper');
-    wrapper.style.height =  getComputedStyle(img).height;
-    wrapper.style.width = getComputedStyle(img).width;
-    let arrows = document.querySelector("#c1Arrows");
-    let arrowsBottom = parseInt(getComputedStyle(img).height)*0.1;
-    arrows.style.bottom = arrowsBottom + "px";
-    let c1Text = document.querySelector("#c1Text");
-    c1Text.style.bottom = arrowsBottom + arrows.offsetHeight + "px";
-    c1Text.style.width = img.offsetWidth - window.innerWidth*0.05 + "px";
-
-    let renome2 = document.querySelector('#footerLogo');
-    renome2.style.paddingLeft = (window.innerWidth*0.96 - parseInt(getComputedStyle(renome2).width))/2 +"px";
-    let footerIcons = document.querySelector('#footerIcons');
-    footerIcons.style.paddingLeft = (window.innerWidth*0.96 - parseInt(getComputedStyle(footerIcons).width))/2 +"px";
-
-    let c2Img2 = document.querySelector('#c2Img2');
-    c2Img2.style.bottom =  parseInt(getComputedStyle(c2Img2).height)*0.21+"px";
-}   
 
 function get_menu(bars){
-    if(bars.dataset.state == "inactive"){
-        document.querySelector('#menu').style.display = "inline-block";
-        document.querySelector('#collage1').style.display = "none";
-        bars.dataset.state = "active";
-        document.querySelector("#liMenu").style.color = "#985E5B";
+    let menu = document.getElementById('menu');
+    if(bars.dataset.state == "closed"){
+        menu.style.opacity = "1";
+        menu.style.top = "100px";
+        document.getElementById("limenu").style.color = "#985E5B";
         bars.dataset.active = "liMenu";
-        document.querySelector("#home").style.height = document.querySelector('ul.menu').offsetHeight + 16 + "px";
+        bars.dataset.state = "open";
     }
-    else if(bars.dataset.state == "active"){
-        document.querySelector('#menu').style.display = "none";
-        document.querySelector('#collage1').style.display = "block";
-        bars.dataset.state = "inactive";
-        document.querySelector("#home").style.height = document.querySelector('div.collage1').offsetHeight + "px";
-        document.querySelector("#liMenu").style.color = "black";
-        bars.dataset.active = "liHome";
+    else if(bars.dataset.state == "open"){
+        menu.style.opacity = "0";
+        menu.style.top = "80px";
+        bars.dataset.state = "closed";
+        document.getElementById("limenu").style.color = "black";
     }
 }
 
@@ -55,13 +24,12 @@ function get_submenu(div){
     let liID = divID.slice(3); 
     for(i = 0; i < liArray["length"]; i++){
         liArray[i].style.display = "none";
-        console.log(liArray[i].id);
         if(liArray[i].id.slice(1) == liID+"-submenu" || liArray[i].id == "li"+liID){
             liArray[i].style.display = "block";
         }
     } 
     let arrowContainer = document.querySelector("#arrowContainerFeatures")
-    
+    document.getElementById('menu').style.height = "1200px";
     arrowContainer.style.left = "3%";
     arrowContainer.style.right = "auto";
     document.querySelector("#arrowFeatures").className = "fas fa-angle-left li-arrow";
