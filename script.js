0e6c37d8664521dfb0b4ad766ad991cbfee59dff
VSlobolinskyi <vitalii.slobolinskyi@gmail.com>
2020-10-26 12:22:05 +0200

    Finshied
    Cleaned styles and optimized JS, added cart menu and mede proper behaviour both for cart menu and naviagation menu

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
-
-
-function get_menu(bars){
-    let menu = document.getElementById('menu');
-    if(bars.dataset.state == "closed"){
-        menu.style.opacity = "1";
-        menu.style.top = "100px";
-        document.getElementById("limenu").style.color = "#985E5B";
-        bars.dataset.active = "liMenu";
-        bars.dataset.state = "open";
+function toggle_cartMenu(){
+    let cartMenu = document.getElementById("cartMenu")
+    if(cartMenu.classList.contains("cart-menu--hidden")){
+        cartMenu.classList.remove("cart-menu--hidden");
+        cartMenu.classList.add("cart-menu--visible");
     }
-    else if(bars.dataset.state == "open"){
-        menu.style.opacity = "0";
-        menu.style.top = "80px";
-        bars.dataset.state = "closed";
-        document.getElementById("limenu").style.color = "black";
+    else if(cartMenu.classList.contains("cart-menu--visible")){
+        cartMenu.classList.remove("cart-menu--visible");
+        cartMenu.classList.add("cart-menu--hidden");
     }
 }
 
-function get_submenu(div){
-    document.querySelector("#searchContainer").style.display = "none";
-    var liArray = document.getElementsByClassName("menu-item");
-    let divID = div.id;
-    let liID = divID.slice(3); 
-    for(i = 0; i < liArray["length"]; i++){
-        liArray[i].style.display = "none";
-        if(liArray[i].id.slice(1) == liID+"-submenu" || liArray[i].id == "li"+liID){
-            liArray[i].style.display = "block";
-        }
+function toggle_menu(){
+    let menu = document.getElementById("menu");
+    if(menu.classList.contains("menu--hidden")){
+        menu.classList.remove("menu--hidden");
+        menu.classList.add("menu--visible");
+    }
+    else if(menu.classList.contains("menu--visible")){
+        menu.classList.remove("menu--visible");
+        menu.classList.add("menu--hidden");
+        setTimeout(reset_menu, 400);
+    }
+}
+function reset_menu(){
+    let submenuItems = document.getElementsByClassName("menu__submenu-item");
+    for(i = 0; i < submenuItems.length; i++){
+        submenuItems[i].classList.add("item--hidden");
+        submenuItems[i].classList.remove("item--visible");
+    }
+    let menuItems = document.getElementsByName("rootItem");
+    for(i = 0; i < menuItems.length; i++){
+        menuItems[i].classList.remove("item--hidden");
+        menuItems[i].classList.add("item--visible");
+    }
+    document.getElementById("back").classList.remove("item--visible");
+    document.getElementById("back").classList.add("item--hidden");
+}
+function open_submenu(item){
+    itemsToHide = document.getElementsByName(item.getAttribute("name"));
+    for(i = 0; i < itemsToHide.length; i++){
+        itemsToHide[i].classList.remove("item--visible")
+        itemsToHide[i].classList.add("item--hidden");
     } 
-    let arrowContainer = document.querySelector("#arrowContainerFeatures")
-    document.getElementById('menu').style.height = "1200px";
-    arrowContainer.style.left = "3%";
-    arrowContainer.style.right = "auto";
-    document.querySelector("#arrowFeatures").className = "fas fa-angle-left li-arrow";
-    document.querySelector("#lifeatures").style.textAlign = "right";
-    document.querySelector("#lifeatures").innerText = "BACK  ";
-    document.querySelector("#home").style.height = document.querySelector("#menu").offsetHeight + "px";
-
-
+    document.getElementById("back").classList.remove("item--hidden")
+    document.getElementById("back").classList.add("item--visible")
+    itemsToShow = document.getElementsByName(item.getAttribute("id"))
+    for(i = 0; i < itemsToShow.length; i++){
+        itemsToShow[i].classList.remove("item--hidden");
+        itemsToShow[i].classList.add("item--visible");
+    } 
+}
+function close_submenu(){
+    let itemsToHide = document.getElementsByClassName("menu__submenu-item item--visible");
+    itemsToHide = document.getElementsByName(itemsToHide[0].getAttribute("name"));
+    for(i = 0; i < itemsToHide.length; i++){
+        itemsToHide[i].classList.remove("item--visible");
+        itemsToHide[i].classList.add("item--hidden");
+    } 
+    let itemToShow = document.getElementById(itemsToHide[0].getAttribute("name"));
+    let itemsToShow = document.getElementsByName(itemToShow.getAttribute("name"));
+    if(itemToShow.getAttribute("name") == "rootItem"){
+        document.getElementById("back").classList.remove("item--visible")
+        document.getElementById("back").classList.add("item--hidden")
+    }
+    for(i = 0; i < itemsToShow.length; i++){
+        itemsToShow[i].classList.remove("item--hidden");
+        itemsToShow[i].classList.add("item--visible");
+    } 
 }
 function input_active(input){
     document.querySelector('#searchIcon').style.display = "none";
