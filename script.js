bfc01f162b7f720e85826d26a50c51d0fb1374b2
VSlobolinskyi <vitalii.slobolinskyi@gmail.com>
2020-10-27 16:06:15 +0200

    Finished v2
    Made proper toggles, cleaned all unnecesarry ids, added comtainers for content inside menu.

--- script.js
+++ script.js
@@ -1,76 +1,51 @@
 function toggle_cartMenu(){
-    let cartMenu = document.getElementById("cartMenu")
-    if(cartMenu.classList.contains("cart-menu--hidden")){
-        cartMenu.classList.remove("cart-menu--hidden");
-        cartMenu.classList.add("cart-menu--visible");
-    }
-    else if(cartMenu.classList.contains("cart-menu--visible")){
-        cartMenu.classList.remove("cart-menu--visible");
-        cartMenu.classList.add("cart-menu--hidden");
-    }
+    document.getElementById("cartMenu").classList.toggle("cart-menu--visible")
 }
 
 function toggle_menu(){
-    let menu = document.getElementById("menu");
-    if(menu.classList.contains("menu--hidden")){
-        menu.classList.remove("menu--hidden");
-        menu.classList.add("menu--visible");
-    }
-    else if(menu.classList.contains("menu--visible")){
-        menu.classList.remove("menu--visible");
-        menu.classList.add("menu--hidden");
-        setTimeout(reset_menu, 400);
-    }
+    document.getElementById("menu").classList.toggle("menu--visible");
+    setTimeout(reset_menu, 400);
 }
 function reset_menu(){
-    let submenuItems = document.getElementsByClassName("menu__submenu-item");
-    for(i = 0; i < submenuItems.length; i++){
-        submenuItems[i].classList.add("item--hidden");
-        submenuItems[i].classList.remove("item--visible");
-    }
-    let menuItems = document.getElementsByName("rootItem");
-    for(i = 0; i < menuItems.length; i++){
-        menuItems[i].classList.remove("item--hidden");
-        menuItems[i].classList.add("item--visible");
-    }
-    document.getElementById("back").classList.remove("item--visible");
-    document.getElementById("back").classList.add("item--hidden");
+    let clasterToHide = document.getElementsByClassName("item-claster--visible");
+    clasterToHide[0].classList.add("item-claster--hidden");
+    clasterToHide[0].classList.remove("item-claster--visible");
+    let clasterToShow = document.getElementById("root");
+    clasterToShow.classList.remove("item-claster--hidden");
+    clasterToShow.classList.add("item-claster--visible");
+    document.getElementById("back").classList.remove("back--visible");
+    document.getElementById("back").classList.add("back--hidden");
 }
 function open_submenu(item){
-    itemsToHide = document.getElementsByName(item.getAttribute("name"));
-    for(i = 0; i < itemsToHide.length; i++){
-        itemsToHide[i].classList.remove("item--visible")
-        itemsToHide[i].classList.add("item--hidden");
-    } 
-    document.getElementById("back").classList.remove("item--hidden")
-    document.getElementById("back").classList.add("item--visible")
-    itemsToShow = document.getElementsByName(item.getAttribute("id"))
-    for(i = 0; i < itemsToShow.length; i++){
-        itemsToShow[i].classList.remove("item--hidden");
-        itemsToShow[i].classList.add("item--visible");
-    } 
+    let clasterToHide = document.getElementsByClassName("item-claster--visible");
+    clasterToHide[0].classList.add("item-claster--hidden");
+    clasterToHide[0].classList.remove("item-claster--visible");
+    let clasterToShow = document.getElementById(item.getAttribute("name"));
+    clasterToShow.classList.remove("item-claster--hidden");
+    clasterToShow.classList.add("item-claster--visible");
+    document.getElementById("back").classList.remove("back--hidden")
+    document.getElementById("back").classList.add("back--visible")
 }
 function close_submenu(){
-    let itemsToHide = document.getElementsByClassName("menu__submenu-item item--visible");
-    itemsToHide = document.getElementsByName(itemsToHide[0].getAttribute("name"));
-    for(i = 0; i < itemsToHide.length; i++){
-        itemsToHide[i].classList.remove("item--visible");
-        itemsToHide[i].classList.add("item--hidden");
-    } 
-    let itemToShow = document.getElementById(itemsToHide[0].getAttribute("name"));
-    let itemsToShow = document.getElementsByName(itemToShow.getAttribute("name"));
-    if(itemToShow.getAttribute("name") == "rootItem"){
-        document.getElementById("back").classList.remove("item--visible")
-        document.getElementById("back").classList.add("item--hidden")
+    let activeClaster = document.getElementsByClassName("item-claster--visible");
+    let clasterToShow = document.getElementById(activeClaster[0].getAttribute("name"));
+    if(activeClaster[0].getAttribute("name") == "root"){
+        document.getElementById("back").classList.add("back--hidden");
+        document.getElementById("back").classList.remove("back--visible");
     }
-    for(i = 0; i < itemsToShow.length; i++){
-        itemsToShow[i].classList.remove("item--hidden");
-        itemsToShow[i].classList.add("item--visible");
-    } 
+    activeClaster[0].classList.add("item-claster--hidden");
+    activeClaster[0].classList.remove("item-claster--visible");
+    clasterToShow.classList.remove("item-claster--hidden");
+    clasterToShow.classList.add("item-claster--visible");
+    
+    
 }
-function input_active(input){
-    document.querySelector('#searchIcon').style.display = "none";
+function input_active(){
+    document.getElementById('searchIcon').classList.remove("search__icon--visible");
+    document.getElementById('searchIcon').classList.add("search__icon--hidden");
+    
 }
-function input_inactive(input){
-    document.querySelector('#searchIcon').style.display = "inline";
+function input_inactive(){
+    document.getElementById('searchIcon').classList.add("search__icon--visible");
+    document.getElementById('searchIcon').classList.remove("search__icon--hidden");
 }
