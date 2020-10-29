a2883d945ea590f68c816651069affb7e188486a
VSlobolinskyi <vitalii.slobolinskyi@gmail.com>
2020-10-29 15:50:46 +0200

    Removed names from divs and changed js accordingly

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+var submenuOrder = ["root"];
+
 function toggle_cart_menu(){
     document.getElementById("cartMenu").classList.remove("cart-menu--undisplay");
-    document.getElementById("cartMenu").classList.toggle("cart-menu--visible")
+    document.getElementById("cartMenu").classList.toggle("cart-menu--visible");
     setTimeout(cart_menu_undisplay, 400);
 }
 function cart_menu_undisplay(){
@@ -8,7 +10,6 @@ function cart_menu_undisplay(){
         document.getElementById("cartMenu").classList.add("cart-menu--undisplay");
     }
 }
-
 function toggle_menu(){
     document.getElementById("menu").classList.remove("menu--undisplay");
     document.getElementById("menu").classList.toggle("menu--visible");
@@ -17,44 +18,44 @@ function toggle_menu(){
     }
 }
 function reset_menu(){
-    let clusterToHide = document.getElementsByClassName("item-cluster--visible");
-    clusterToHide[0].classList.add("item-cluster--hidden");
-    clusterToHide[0].classList.remove("item-cluster--visible");
+    let clusterToHide = document.getElementById(submenuOrder[submenuOrder.length-1]);
+    clusterToHide.classList.add("item-cluster--hidden");
+    clusterToHide.classList.remove("item-cluster--visible");
     let clusterToShow = document.getElementById("root");
     clusterToShow.classList.remove("item-cluster--hidden");
     clusterToShow.classList.add("item-cluster--visible");
     document.getElementById("back").classList.remove("back--visible");
     document.getElementById("back").classList.add("back--hidden");
     document.getElementById("menu").classList.add("menu--undisplay");
+    submenuOrder = ["root"];
 }
 function open_submenu(item){
-    let clusterToHide = document.getElementsByClassName("item-cluster--visible");
-    clusterToHide[0].classList.add("item-cluster--hidden");
-    clusterToHide[0].classList.remove("item-cluster--visible");
-    let clusterToShow = document.getElementById(item.getAttribute("name"));
+    let clusterToHide = document.getElementById(submenuOrder[submenuOrder.length-1]);
+    clusterToHide.classList.add("item-cluster--hidden");
+    clusterToHide.classList.remove("item-cluster--visible");
+    submenuOrder.push(item.getAttribute("id")+"Submenu");
+    let clusterToShow = document.getElementById(submenuOrder[submenuOrder.length-1]);
     clusterToShow.classList.remove("item-cluster--hidden");
     clusterToShow.classList.add("item-cluster--visible");
-    document.getElementById("back").classList.remove("back--hidden")
-    document.getElementById("back").classList.add("back--visible")
+    document.getElementById("back").classList.remove("back--hidden");
+    document.getElementById("back").classList.add("back--visible");
 }
 function close_submenu(){
-    let activecluster = document.getElementsByClassName("item-cluster--visible");
-    let clusterToShow = document.getElementById(activecluster[0].getAttribute("name"));
-    if(activecluster[0].getAttribute("name") == "root"){
+    let activecluster = document.getElementById(submenuOrder[submenuOrder.length-1]);
+    let clusterToShow = document.getElementById(submenuOrder[submenuOrder.length-2]);
+    if(submenuOrder[submenuOrder.length-2] == "root"){
         document.getElementById("back").classList.add("back--hidden");
         document.getElementById("back").classList.remove("back--visible");
     }
-    activecluster[0].classList.add("item-cluster--hidden");
-    activecluster[0].classList.remove("item-cluster--visible");
+    activecluster.classList.add("item-cluster--hidden");
+    activecluster.classList.remove("item-cluster--visible");
     clusterToShow.classList.remove("item-cluster--hidden");
     clusterToShow.classList.add("item-cluster--visible");
-    
-    
+    submenuOrder.pop();
 }
 function input_active(){
     document.getElementById('searchIcon').classList.remove("search__icon--visible");
     document.getElementById('searchIcon').classList.add("search__icon--hidden");
-    
 }
 function input_inactive(){
     document.getElementById('searchIcon').classList.add("search__icon--visible");
