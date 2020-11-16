001d10495c65f2027e9d21c5f4f712d27842c1f3
VSlobolinskyi <vitalii.slobolinskyi@gmail.com>
2020-11-16 15:21:26 +0200

    Added closure nad global vars

--- script.js
+++ script.js
@@ -1,92 +1,99 @@
-var submenuOrder = ["root"];
+function closure(){
+  var submenuOrder = ["root"];
+  var cartMenu = document.getElementById("cartMenu");
+  var menu = document.getElementById("menu");
+  var back = document.getElementById("back");
+  var cross = document.getElementById("cross");
+  var bars = document.getElementById("bars");
+  var searchIcon = document.getElementById('searchIcon');
 
-function toggleCartMenu() {
-  cartMenu = document.getElementById("cartMenu");
-  cartMenu.classList.remove("cart-menu--hidden");
-  cartMenu.classList.toggle("cart-menu--fade-in");
-  setTimeout(cartMenuUndisplay, 400);
-}
-
-function cartMenuUndisplay() {
-  cartMenu = document.getElementById("cartMenu");
-  if (!cartMenu.classList.contains("cart-menu--fade-in")) {
-    cartMenu.classList.add("cart-menu--hidden");
+  function toggleCartMenu() {
+    cartMenu.classList.remove("cart-menu--hidden");
+    cartMenu.classList.toggle("cart-menu--fade-in");
+    function cartMenuUndisplay() {
+      cartMenu = document.getElementById("cartMenu");
+      if (!cartMenu.classList.contains("cart-menu--fade-in")) {
+        cartMenu.classList.add("cart-menu--hidden");
+      }
+    }
+    setTimeout(cartMenuUndisplay, 400);
   }
-}
 
-function openMenu() {
-  let menu = document.getElementById("menu");
-  let cross = document.getElementById("cross");
-  let bars = document.getElementById("bars");
-  cross.classList.toggle("icons--hidden");
-  bars.classList.toggle("icons--hidden");
-  menu.classList.remove("menu--hidden");
-  menu.classList.toggle("menu--fade-in");
-}
+  function openMenu() {
+    cross.classList.toggle("icons--hidden");
+    bars.classList.toggle("icons--hidden");
+    menu.classList.remove("menu--hidden");
+    menu.classList.toggle("menu--fade-in");
+  }
 
-function closeMenu() {
-  let menu = document.getElementById("menu")
-  let cross = document.getElementById("cross");
-  let bars = document.getElementById("bars");
-  cross.classList.toggle("icons--hidden");
-  bars.classList.toggle("icons--hidden");
-  menu.classList.toggle("menu--fade-in");
-  setTimeout(resetMenu, 400);
-}
+  function closeMenu() {
+    cross.classList.toggle("icons--hidden");
+    bars.classList.toggle("icons--hidden");
+    menu.classList.toggle("menu--fade-in");
+    function resetMenu() {
+      var clasterToHide = document.getElementById(submenuOrder[submenuOrder.length - 1]);
+      var clasterToShow = document.getElementById("root");
+      clasterToHide.classList.add("item-claster--hidden");
+      clasterToHide.classList.remove("item-claster--visible");
+      clasterToHide.classList.remove("item-claster--submenu");
+      clasterToShow.classList.add("item-claster--visible");
+      clasterToShow.classList.remove("item-claster--hidden");
+      back.classList.add("back--hidden");
+      document.getElementById("menu").classList.add("menu--hidden");
+      submenuOrder = ["root"];
+    }
+    setTimeout(resetMenu, 400);
+  }
 
-function resetMenu() {
-  console.log("Here");
-  let clasterToHide = document.getElementById(submenuOrder[submenuOrder.length - 1]);
-  let back = document.getElementById("back");
-  let clasterToShow = document.getElementById("root");
-  clasterToHide.classList.add("item-claster--hidden");
-  clasterToHide.classList.remove("item-claster--visible");
-  clasterToHide.classList.remove("item-claster--submenu");
-  clasterToShow.classList.add("item-claster--visible");
-  clasterToShow.classList.remove("item-claster--hidden");
-  back.classList.add("back--hidden");
-  document.getElementById("menu").classList.add("menu--hidden");
-  submenuOrder = ["root"];
-}
+  function openSubmenu(itemId) {
+    submenuOrder.push(itemId+"Submenu");
+    var clasterToShow = document.getElementById(submenuOrder[submenuOrder.length - 1]);
+    var clasterToHide = document.getElementById(submenuOrder[submenuOrder.length - 2]);
+    clasterToHide.classList.add("item-claster--hidden");
+    clasterToHide.classList.remove("item-claster--visible");
+    clasterToHide.classList.remove("item-claster--submenu");
+    clasterToShow.classList.add("item-claster--visible");
+    clasterToShow.classList.remove("item-claster--hidden");
+    clasterToShow.classList.add("item-claster--submenu");
+    back.classList.remove("back--hidden");
+    back.classList.add("back--visible");
+  }
 
-function openSubmenu(item) {
-  console.log(item.getAttribute("id"))
-  submenuOrder.push(item.getAttribute("id") + "Submenu");
-  let clasterToShow = document.getElementById(submenuOrder[submenuOrder.length - 1]);
-  let clasterToHide = document.getElementById(submenuOrder[submenuOrder.length - 2]);
-  let back = document.getElementById("back");
-  clasterToHide.classList.add("item-claster--hidden");
-  clasterToHide.classList.remove("item-claster--visible");
-  clasterToHide.classList.remove("item-claster--submenu");
-  clasterToShow.classList.add("item-claster--visible");
-  clasterToShow.classList.remove("item-claster--hidden");
-  clasterToShow.classList.add("item-claster--submenu");
-  back.classList.remove("back--hidden");
-  back.classList.add("back--visible");
-}
-
-function closeSubmenu() {
-  let clasterToHide = document.getElementById(submenuOrder[submenuOrder.length - 1]);
-  let clasterToShow = document.getElementById(submenuOrder[submenuOrder.length - 2]);
-  clasterToHide.classList.add("item-claster--hidden");
-  clasterToHide.classList.remove("item-claster--visible");
-  clasterToShow.classList.remove("item-claster--submenu");
-  clasterToShow.classList.add("item-claster--visible");
-  clasterToShow.classList.remove("item-claster--hidden");
-  clasterToShow.classList.add("item-claster--submenu");
-  if (submenuOrder[submenuOrder.length - 2] == "root") {
-    document.getElementById("back").classList.toggle("back--hidden");
+  function closeSubmenu() {
+    var clasterToHide = document.getElementById(submenuOrder[submenuOrder.length - 1]);
+    var clasterToShow = document.getElementById(submenuOrder[submenuOrder.length - 2]);
+    clasterToHide.classList.add("item-claster--hidden");
+    clasterToHide.classList.remove("item-claster--visible");
     clasterToShow.classList.remove("item-claster--submenu");
+    clasterToShow.classList.add("item-claster--visible");
+    clasterToShow.classList.remove("item-claster--hidden");
+    clasterToShow.classList.add("item-claster--submenu");
+    if (submenuOrder[submenuOrder.length - 2] == "root") {
+      document.getElementById("back").classList.toggle("back--hidden");
+      clasterToShow.classList.remove("item-claster--submenu");
+    }
+    submenuOrder.pop();
   }
-  submenuOrder.pop();
+
+  function inputActive() {
+    searchIcon.classList.toggle("search__icon--hidden");
+  }
+
+  function inputInactive() {
+    searchIcon.classList.toggle("search__icon--hidden");
+  }
+  return [toggleCartMenu, openMenu, closeMenu, openSubmenu, closeSubmenu, inputActive, inputInactive];
 }
 
-function inputActive() {
-  let searchIcon = document.getElementById('searchIcon');
-  searchIcon.classList.toggle("search__icon--hidden");
-}
 
-function inputInactive() {
-  let searchIcon = document.getElementById('searchIcon');
-  searchIcon.classList.toggle("search__icon--hidden");
-}
+var functionArray = closure();
+
+document.getElementById("cart").addEventListener("click", functionArray[0]);
+document.getElementById("bars").addEventListener("click", functionArray[1]);
+document.getElementById("cross").addEventListener("click", functionArray[2]);
+document.getElementById("features").addEventListener("click", function() {
+  functionArray[3]("features");
+});
+document.getElementById("back").addEventListener("click", functionArray[4]);
+document.getElementById("search").addEventListener("onfocus", functionArray[5]);
+document.getElementById("search").addEventListener("onblur", functionArray[6]);
